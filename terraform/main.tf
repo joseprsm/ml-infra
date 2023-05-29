44d945a6bbca9c51ac23c25d7f7cbe47cca8a1f2
José Medeiros <joseprsm@gmail.com>
2023-05-29 20:42:51 +0100

    add kustomization provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,16 @@
+terraform {
+  required_providers {
+    kustomization = {
+      source  = "kbst/kustomize"
+      version = "0.2.0-beta.3"
+    }
+  }
+  required_version = ">= 0.12"
+}
+
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
 }
+
+provider "kustomization" {}
