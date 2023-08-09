bb71fb8233d01057bee9201aa744b8514a3649cd
José Medeiros <joseprsm@gmail.com>
2023-08-09 23:31:22 +0100

    install argo workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,9 @@ module "argocd" {
   name = "argocd"
   url = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
 }
+
+module "argo_workflows" {
+  source = "./modules/install"
+  name = "argo"
+  url = "https://github.com/argoproj/argo-workflows/releases/download/v3.4.9/install.yaml"
+}
