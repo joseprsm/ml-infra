eae54aaa4e6ac437574ef5d89b216fcbc4cc616e
José Medeiros <joseprsm@gmail.com>
2023-08-10 00:15:26 +0100

    remove kustomization

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kustomization = {
-      source  = "kbst/kustomization"
-      version = "0.9.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_path    = var.kube_config
   config_context = "minikube"
